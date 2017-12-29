e4f77b8ae839f5a5cb7eaf1f846f9a71e533a85e
Tom Johnson <tom@dp.la>
2017-12-29 13:58:21 -0800

    Add ordered fields to Collections
    Ordered fields in collections was implemented in Epigaea. It's unclear whether
    this is a requirement--if not, we may need to revert this commit. This work is
    necessary to pass the tests for `epigaea`.

--- lib/tufts/curation/collection.rb
+++ lib/tufts/curation/collection.rb
@@ -9,9 +9,49 @@ module Tufts
         index.as :stored_searchable
       end
 
+      include Tufts::Curation::Schema::Ordered
+
       # This must be included at the end, because it finalizes the metadata
       # schema (by adding accepts_nested_attributes)
       include Hyrax::BasicMetadata
+
+      ##
+      # Overrides setter method to preserve order in a second property.
+      #
+      # @param values [Array<Object] Ordered array of values
+      #
+      # @return [Array<Object>]
+      def creator=(values)
+        super && self.ordered_creators = values.to_json
+      end
+
+      ##
+      # Overrides getter method to return the creators in the correct order.
+      #
+      # @return [Array<Object>]
+      def creator
+        return super if ordered_creators.blank?
+        JSON.parse(ordered_creators)
+      end
+
+      ##
+      # Overrides setter method to preserve order in a second property.
+      #
+      # @param values [Array<Object>] Ordered array of values
+      #
+      # @return [Array<Object>]
+      def description=(values)
+        super && self.ordered_descriptions = values.to_json
+      end
+
+      ##
+      # Overrides getter method to return the descriptions in the correct order.
+      #
+      # @return [Array<Object>]
+      def description
+        return super if ordered_descriptions.blank?
+        JSON.parse(ordered_descriptions)
+      end
     end
   end
 end
