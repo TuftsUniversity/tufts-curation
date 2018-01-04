c6e900fa7b1ea5e336a0eb42ef874241d170c644
Tom Johnson <tom@dp.la>
2018-01-03 17:18:06 -0800

    Set `Hyrax.config.admin_set_predicate` on `model_setup!`
    All client apps should use the same predicate for the `AdminSet` relation. We
    set that automatically as part of the model setup.

--- spec/tufts/curation_spec.rb
+++ spec/tufts/curation_spec.rb
@@ -10,6 +10,7 @@ describe Tufts::Curation do
     let(:configuration_class) do
       # rubocop:disable RSpec/InstanceVariable
       Class.new do
+        attr_accessor :admin_set_predicate
         attr_reader :registered_concerns
 
         def register_curation_concern(*curation_concern_types)
@@ -30,6 +31,12 @@ describe Tufts::Curation do
       expect { described_class.setup_models!(configuration: configuration) }
         .to change { configuration.registered_concerns }
         .to contain_exactly(*expected_concerns)
+    end
+
+    it 'defines configuration#admin_set_predicate' do
+      expect { described_class.setup_models!(configuration: configuration) }
+        .to change { configuration.admin_set_predicate }
+        .to Tufts::Vocab::Tufts.admin_set_member
     end
 
     it 'defines Collection' do
