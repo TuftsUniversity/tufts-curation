ee22d3df2a342b0bc92bc431f75f9f03fa112988
Travis Lilleberg <travis.lilleberg@tufts.edu>
2018-03-27 15:28:38 -0400

    Fixed rubocop errors

--- lib/tufts/curation/voting_record_indexer.rb
+++ lib/tufts/curation/voting_record_indexer.rb
@@ -6,6 +6,8 @@ module Tufts
       # The Nokogiri document
       attr_accessor :noko
 
+      # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/MethodLength
       def generate_solr_document
         super.tap do |solr_doc|
           # Only do this after the indexer has the file_set
@@ -13,19 +15,19 @@ module Tufts
             load_elections_xml(object)
 
             solr_doc['voting_record_xml_tesi'] = @noko.to_xml
-            solr_doc['format_ssim'] = 'Election Record' #solr_doc['format_tesim']
-            solr_doc['title_ssi'] = solr_doc['title_tesim'].first #solr_doc['title_tesi']
+            solr_doc['format_ssim'] = 'Election Record' # solr_doc['format_tesim']
+            solr_doc['title_ssi'] = solr_doc['title_tesim'].first # solr_doc['title_tesi']
 
             solr_doc['party_affiliation_sim'] = get_all_vs('//aas:candidate/@affiliation', '//aas:elector/@affiliation')
             solr_doc['party_affiliation_id_ssim'] = get_all_vs('//aas:candidate/@affiliation_id', '//aas:elector/@affiliation_id')
-            #solr_doc['party_affiliation_id_ssim'].delete_if { |party_id| Party.find(party_id).nil? }
+            # solr_doc['party_affiliation_id_ssim'].delete_if { |party_id| Party.find(party_id).nil? }
 
             solr_doc['date_tesim'] = get_all_vs('/aas:election_record/@date')
             solr_doc['date_isi'] = solr_doc['date_tesim'].map(&:to_i).first
             # solr_doc["date_sim"] = date.first[0..3] unless date.first.nil?
 
             solr_doc['office_id_ssim'] = get_v('/aas:election_record/aas:office/@office_id')
-            solr_doc['office_role_title_tesim'] = get_all_vs( '//aas:role/@title')
+            solr_doc['office_role_title_tesim'] = get_all_vs('//aas:role/@title')
             solr_doc['office_name_tesim'] = get_authority_from_nnv(solr_doc['office_id_ssim'], "office")
 
             solr_doc['state_name_tesim'] = solr_doc['state_name_sim'] = get_all_vs('//aas:admin_unit[@type="State"]/@name')
@@ -47,85 +49,87 @@ module Tufts
           end
         end # End super.tap
       end
+      # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/MethodLength
       # End def generate_solr_document
 
       private
 
-      ##
-      # Gets the first value from a place.
-      def get_v(xpath)
-        @noko.xpath(xpath).first.value
-      end
-
-      ##
-      # Gets all the values from a node set as strings in an array.
-      #
-      # @param {str} xpaths
-      #   The xpath strings to search
-      def get_all_vs(*xpaths)
-        if xpaths.length.zero?
-          Rails.logger.info("You need to pass at least one argument to get_all_vs!")
-          return []
+        ##
+        # Gets the first value from a place.
+        def get_v(xpath)
+          @noko.xpath(xpath).first.value
         end
 
-        all_values = []
-        xpaths.each do |path|
-          @noko.xpath(path).each { |node| all_values << node.value }
-        end
-        all_values - ['null']
-      end
-
-      ##
-      # Gets all the text and makes it one, like Voltron.
-      #
-      # @param {Hash} doc
-      #   The solr document.
-      def get_all_text(doc)
-        all_text_values = []
-        undesirables = ['voting_record_xml_tesi', 'object_profile_ssm']
-
-        doc.each do |key, value|
-          all_text_values << value unless undesirables.include?(key)
-        end
-
-        all_text_values.flatten.uniq - ["null", ""]
-      end
-
-      # def office_name(id)
-      #   Office.find(id).blank? ? nil : office.name
-      # end
-
-      ##
-      # Loads the elections xml file into @noko
-      #
-      # @param {obj} active_fedora_obj
-      #   The object from ActiveFedora
-      def load_elections_xml(active_fedora_obj)
-        active_fedora_obj.file_sets.each do |file_set|
-          f = file_set.original_file
-          begin
-            xml = Nokogiri::XML(f.content)
-            next unless xml.xpath('/*').first.name == 'election_record'
-          rescue
-            next
+        ##
+        # Gets all the values from a node set as strings in an array.
+        #
+        # @param {str} xpaths
+        #   The xpath strings to search
+        def get_all_vs(*xpaths)
+          if xpaths.length.zero?
+            Rails.logger.info("You need to pass at least one argument to get_all_vs!")
+            return []
           end
-          @noko = xml
-        end # end each file set
-      end
 
-      ##
-      # Sends the api request to the NNV site to get authorities
-      #
-      # @param {str} q
-      #   The search string, an id for office or party, a name for state.
-      # @param {str} auth
-      #   The authority to search: office, party, or state.
-      def get_authority_from_nnv(q, auth)
-        base_url = "http://elections-prod-01.lib.tufts.edu/qa/search"
-        uri = URI.parse("#{base_url}/#{auth}/subjects?q=#{q}")
-        response = Net::HTTP.get_response(uri)
-        response.code == "200" ? response.body : q
-      end
+          all_values = []
+          xpaths.each do |path|
+            @noko.xpath(path).each { |node| all_values << node.value }
+          end
+          all_values - ['null']
+        end
+
+        ##
+        # Gets all the text and makes it one, like Voltron.
+        #
+        # @param {Hash} doc
+        #   The solr document.
+        def get_all_text(doc)
+          all_text_values = []
+          undesirables = ['voting_record_xml_tesi', 'object_profile_ssm']
+
+          doc.each do |key, value|
+            all_text_values << value unless undesirables.include?(key)
+          end
+
+          all_text_values.flatten.uniq - ["null", ""]
+        end
+
+        # def office_name(id)
+        #   Office.find(id).blank? ? nil : office.name
+        # end
+
+        ##
+        # Loads the elections xml file into @noko
+        #
+        # @param {obj} active_fedora_obj
+        #   The object from ActiveFedora
+        def load_elections_xml(active_fedora_obj)
+          active_fedora_obj.file_sets.each do |file_set|
+            f = file_set.original_file
+            begin
+              xml = Nokogiri::XML(f.content)
+              next unless xml.xpath('/*').first.name == 'election_record'
+            rescue
+              next
+            end
+            @noko = xml
+          end # end each file set
+        end
+
+        ##
+        # Sends the api request to the NNV site to get authorities
+        #
+        # @param {str} q
+        #   The search string, an id for office or party, a name for state.
+        # @param {str} auth
+        #   The authority to search: office, party, or state.
+        def get_authority_from_nnv(q, auth)
+          base_url = "http://elections-prod-01.lib.tufts.edu/qa/search"
+          uri = URI.parse("#{base_url}/#{auth}/subjects?q=#{q}")
+          response = Net::HTTP.get_response(uri)
+          response.code == "200" ? response.body : q
+        end
     end # End class VotingRecordIndexer
   end
 end
