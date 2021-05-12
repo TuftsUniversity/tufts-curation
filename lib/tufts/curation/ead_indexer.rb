175e7482b5ca6982379ff93d1d86afdd60d2547d
Mike Korcynski <mkorcy@gmail.com>
2021-05-12 11:26:35 -0400

    Ead indexing (#3)
    * WIP ead indexing updates
    
    * remove EadHelper reference
    
    * unwind class methods
    
    * refactor how id is accessed.
    
    * WIP indexing new fields
    
    * WIP getting the solr_doc right
    
    * fields are overwriting the primary metadatafields
    
    * trying to deal with overwriting values vs nils
    
    * don't index title
    
    * cops

--- lib/tufts/curation/ead_indexer.rb
+++ lib/tufts/curation/ead_indexer.rb
@@ -2,11 +2,31 @@
 
 module Tufts
   module Curation
+    # rubocop:disable Metrics/ClassLength
     class EadIndexer < Tufts::Curation::Indexer
       # The Nokogiri document
       attr_accessor :noko
+
+      # rubocop:disable Metrics/MethodLength
       def generate_solr_document
         super.tap do |solr_doc|
+          results = {}
+          id = object.id
+          find_indexable_fields(id, results)
+          # puts "#{results}"
+          results.each do |field_name, field_values|
+            # puts("  #{field_name}")
+            if solr_doc["#{field_name}_tesim"].nil?
+              solr_doc["#{field_name}_tesim"] = field_values
+            else
+              solr_doc["#{field_name}_tesim"] += field_values
+            end
+            # [object.file_sets[0].characterization_proxy.format_label] if object.file_sets && !object.file_sets.empty?
+
+            # field_values.each do |field_value|
+            #    puts("    #{field_value}")
+            # end
+          end
           # Only do this after the indexer has the file_set
           unless object.file_sets.nil?
             load_ead_xml(object)
@@ -37,6 +57,103 @@ module Tufts
             xml.remove_namespaces!
             @noko = xml
           end # end each file set
+        end
+
+        def find_indexable_fields(id, results)
+          document_fedora = ActiveFedora::Base.find(id)
+          document_ead = Nokogiri::XML(document_fedora.file_sets.first.original_file.content)
+          document_ead.remove_namespaces!
+
+          archdescs = document_ead.xpath('//ead/archdesc')
+
+          return false if archdescs.empty?
+
+          archdescs.each do |archdesc|
+            find_controlaccess(archdesc, results)
+            find_dsc(archdesc, results)
+          end
+
+          # results is a hash table whose keys are field names and values are hash tables with field values as both key and value, like:
+          # {"genreform"=>{"Diaries"=>"Diaries", "Sketchbooks"=>"Sketchbooks"}, "subject"=>{"Adolescence"=>"Adolescence", "Advertising"=>"Advertising"}}.
+          # The reason to have hash tables within hash tables is for best insertion performance and so that field values aren't duplicated, but
+          # it's a little confusing as a returned result, so convert the inner hash tables to arrays so that the above example would look like"
+          # {"genreform"=>["Diaries", "Sketchbooks"], "subject"=>["Adolescence", "Advertising"]}.
+          results.each do |field_name, field_values|
+            results[field_name] = field_values.keys
+          end
+
+          true
+        end
+
+        def find_controlaccess(node, results)
+          controlaccess = node.xpath('./controlaccess')
+
+          return false if controlaccess.empty?
+
+          # puts('  ' + node.name + (node.name == 'archdesc' ? '' : (' ' + node['level'] + ' ' + node['id'])))
+          find_tag(controlaccess, 'persname', results)
+          find_tag(controlaccess, 'corpname', results)
+          find_tag(controlaccess, 'famname', results)
+          find_tag(controlaccess, 'geogname', results)
+          find_tag(controlaccess, 'genreform', results)
+          find_tag(controlaccess, 'subject', results)
+          # find_tag(controlaccess, 'title', results)
+
+          true
+        end
+
+        def find_dsc(node, results)
+          dsc = node.xpath('./dsc')
+
+          return false if dsc.empty?
+
+          unless find_series(dsc, results)  # ASpace series (c)
+            find_series(dsc, results, 1)    # non-ASpace series (c01, c02...)
+          end
+
+          true
+        end
+
+        def find_series(node, results, level = 0)
+          if level.zero?
+            level_string = ''
+            next_level = 0
+          else
+            level_string = format('%02d', level)
+            next_level = level + 1
+          end
+
+          serieses = node.xpath('./c' + level_string)
+
+          return false if serieses.empty?
+
+          serieses.each do |series|
+            find_controlaccess(series, results)
+            find_series(series, results, next_level)
+          end
+
+          true
+        end
+
+        def find_tag(node, tag, results)
+          subnodes = node.xpath('./' + tag)
+
+          return false if subnodes.empty?
+
+          # puts('    ' + tag)
+          tag_hash = results[tag]
+
+          if tag_hash.nil?
+            tag_hash = {}
+            results[tag] = tag_hash
+          end
+
+          subnodes.each do |subnode|
+            # puts('      ' + subnode.text)
+            tag_hash[subnode.text] = subnode.text
+          end
+
+          true
         end
       # end private
     end # End class VotingRecordIndexer
