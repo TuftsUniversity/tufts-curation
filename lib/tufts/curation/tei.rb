4dc9e48b60a7ffde88aebe25f61c8afa34d45793
Mike Korcynski <mkorcy@gmail.com>
2021-08-25 12:55:51 -0400

    tei indexing

--- lib/tufts/curation/tei.rb
+++ lib/tufts/curation/tei.rb
@@ -4,6 +4,17 @@ module Tufts
   module Curation
     ##
     # A Tei work type
-    class Tei < TuftsModel; end
+    class Tei < TuftsModel
+      ##
+      # Set indexer inside .inherited because Ead is actually an anonymous class
+      # that inherits Tufts::Curation::Ead
+      #
+      # @argument subclass
+      #   The anonymous class that's being used as Ead
+      def self.inherited(subclass)
+        super
+        subclass.indexer = Tufts::Curation::TeiIndexer
+      end
+    end
   end
 end
---
+++ lib/tufts/curation/tei_indexer.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Tufts
+  module Curation
+    class TeiIndexer < Tufts::Curation::Indexer
+      # The Nokogiri document
+      attr_accessor :noko
+      def generate_solr_document
+        super.tap do |solr_doc|
+          begin
+            # Only do this after the indexer has the file_set
+            unless object.file_sets.nil?
+              load_tei_xml(object)
+              if @noko.nil?
+                Rails.logger.warn("Couldn't find the Ead XML for #{solr_doc['id']}")
+              else
+                solr_doc['all_text_timv'] = @noko.xpath('//text()').text.gsub(/[^0-9A-Za-z]/, ' ')
+              end
+            end
+          rescue NoMethodError => exception
+            Rails.logger.warn("#{exception.class}: #{exception.message}")
+          end
+        end # End super.tap
+      end
+      # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/MethodLength
+      # rubocop:enable Metrics/BlockLength
+      # End def generate_solr_document
+
+      private
+
+        def load_tei_xml(active_fedora_obj)
+          active_fedora_obj.file_sets.each do |file_set|
+            f = file_set.original_file
+            begin
+              xml = Nokogiri::XML(f.content)
+              next unless xml.xpath('/*').first.name.start_with? "TEI"
+            rescue
+              next
+            end
+            xml.remove_namespaces!
+            @noko = xml
+          end # end each file set
+        end
+      # end private
+    end # End class TEIIndexer
+  end
+end
