02a80778438f59fddd2fc3e1f0d0c791cab2416a
Mike Korcynski <mkorcy@gmail.com>
2023-06-29 11:56:29 -0400

    load order matters

--- lib/tufts/curation/tufts_model.rb
+++ lib/tufts/curation/tufts_model.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'hyrax/controlled_vocabularies/location'
 require 'hyrax/controlled_vocabularies/resource_label_caching'
+require 'hyrax/controlled_vocabularies/location'
 require 'models/concerns/hyrax/core_metadata'
 require 'models/concerns/hyrax/basic_metadata'
 
