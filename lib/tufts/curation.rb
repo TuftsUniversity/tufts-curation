61aff097e0de0e094f32841c3fb4d32b73a86c42
raworlando <rynOrlando@gmail.com>
2026-08-31 14:48:04 -0400

    change path look up to hyrax 3

--- lib/tufts/curation.rb
+++ lib/tufts/curation.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 hyrax_path = $LOAD_PATH.find { |path| path.include?('hyrax-c42434073491') }
 hyrax_path = $LOAD_PATH.find { |path| path.include?('hyrax-3') } if hyrax_path.nil?
+hyrax_path = $LOAD_PATH.find { |path| path.include?('hyrax') } if hyrax_path.nil?
 $LOAD_PATH.unshift(Pathname.new(hyrax_path).join('..', 'app').to_s)
 
 require 'active_fedora'
