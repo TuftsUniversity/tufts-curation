812fe080717bfa306bdcf74c3ee38eafa992174d
raworlando <rynOrlando@gmail.com>
2026-09-03 10:33:57 -0400

    No change

--- lib/tufts/curation.rb
+++ lib/tufts/curation.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+# No Change
 hyrax_path = $LOAD_PATH.find { |path| path.include?('hyrax-c42434073491') }
 hyrax_path = $LOAD_PATH.find { |path| path.include?('hyrax-3') } if hyrax_path.nil?
 hyrax_path = $LOAD_PATH.find { |path| path.include?('hyrax') } if hyrax_path.nil?
