de4805f7c5a6e10dd7c7aa75835d328fc9b227fd
raworlando <rynOrlando@gmail.com>
2026-09-21 10:14:52 -0400

    please

--- lib/tufts/curation.rb
+++ lib/tufts/curation.rb
@@ -7,7 +7,7 @@ hyrax_path = $LOAD_PATH.find { |path| path.include?('hyrax') } if hyrax_path.nil
 print(hyrax_path)
 print("Look above here!")
 $LOAD_PATH.unshift(Pathname.new(hyrax_path).to_s)
-# $LOAD_PATH.unshift(Pathname.new(hyrax_path).join('..', 'app').to_s)
+$LOAD_PATH.unshift(Pathname.new(hyrax_path).join('..', 'app').to_s)
 
 require 'logger'
 require 'active_fedora'
