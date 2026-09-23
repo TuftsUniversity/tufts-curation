41b8b905d06a4604ea1cb336404c99442f000c69
raworlando <rynOrlando@gmail.com>
2026-09-23 13:24:15 -0400

    bring back a few

--- lib/tufts/curation/spec/shared_examples/a_tufts_model.rb
+++ lib/tufts/curation/spec/shared_examples/a_tufts_model.rb
@@ -3,7 +3,7 @@
 shared_examples 'a tufts model' do
   subject(:work) { described_class.new }
 
-  # it_behaves_like 'a model with admin metadata attributes'
+  it_behaves_like 'a model with admin metadata attributes'
   it_behaves_like 'a model with descriptive metadata attributes'
   it_behaves_like 'a model with ordered metadata'
   # it_behaves_like 'a model with hyrax basic metadata'
